f6eecc9d37c36fa31dfeb6ceb9969b7d99bf24c7
Lasya <lmallipe0001@mymail.lausd.net>
2026-09-16 19:26:47 -0700

    Add NeighborhoodRunner to execute character painting

---
+++ NeighborhoodRunner.java
@@ -0,0 +1,10 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    CharacterPainter ayana = new CharacterPainter();
+    ayana.setPaint(1000);
+    ayana.paintCharacter();
+  }
+}
